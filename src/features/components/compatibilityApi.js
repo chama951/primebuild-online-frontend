91190250e5f1713fbef07017f852251921ae7d90
Chamara Jeewantha <chamara.j.hitigedara@gmail.com>
2026-01-19 22:23:27 +0530

    Build Cart almost completed.

---
+++ src/features/components/compatibilityApi.js
@@ -0,0 +1,32 @@
+import {baseApi} from "../../services/baseApi";
+
+export const compatibilityApi = baseApi.injectEndpoints({
+    endpoints: (builder) => ({
+
+        getCompatibleItemsByComponent: builder.query({
+            query: ({ componentId, selectedItems }) => {
+                console.log('Sending to backend:', {
+                    componentId,
+                    itemListCount: selectedItems?.length || 0,
+                    selectedItems
+                });
+
+                return {
+                    url: `/compatibility`,
+                    params: { component: componentId },
+                    method: 'POST',
+                    body: {
+                        itemList: selectedItems || [] // Send full item objects
+                    }
+                };
+            },
+            providesTags: (result, error, { componentId }) => [
+                { type: 'CompatibleItems', id: componentId }
+            ],
+        }),
+    }),
+});
+
+export const {
+    useGetCompatibleItemsByComponentQuery,
+} = compatibilityApi;
